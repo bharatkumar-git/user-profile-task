de9a4da7d53da4fa95b2400072eb8896973c51de
Bharat <bharatkumartirumani@mail.com>
2025-09-24 10:09:27 +0530

    Added search filter

--- src/App.jsx
+++ src/App.jsx
@@ -7,7 +7,8 @@ import "./App.css";
 
 const App = () => {
   const [usersInfoList, setUserInfoList] = useState([]);
-  console.log(usersInfoList);
+  const [searchInput, setSearchInput] = useState("");
+  // console.log(usersInfoList);
 
   useEffect(() => {
     const fetchUsers = async () => {
@@ -25,22 +26,42 @@ const App = () => {
     fetchUsers();
   }, []);
 
-  const EachUser = props => {
-    const {userDetails} = props
+  const EachUser = (props) => {
+    const { userDetails } = props;
     return (
-    <li className="app-user-cards-li-item">
-      <p><span className="bold-text">Name:  </span>{userDetails.name}</p>
-      <p><span className="bold-text">Phone:  </span>{userDetails.phone}</p>
-      <p><span className="bold-text">Username:  </span>{userDetails.username}</p>
-      <p><span className="bold-text">Email:  </span>{userDetails.email}</p>
-      <p><span className="bold-text">Website:  </span>{userDetails.website}</p>
-      <hr />
-      <div className="app-user-profile-icons-container">
-        <FaUserEdit size={20} />
-        <IoMdTrash size={20} />
-      </div>
-    </li>
-  )};
+      <li className="app-user-cards-li-item">
+        <p>
+          <span className="bold-text">Name: </span>
+          {userDetails.name}
+        </p>
+        <p>
+          <span className="bold-text">Phone: </span>
+          {userDetails.phone}
+        </p>
+        <p>
+          <span className="bold-text">Username: </span>
+          {userDetails.username}
+        </p>
+        <p>
+          <span className="bold-text">Email: </span>
+          {userDetails.email}
+        </p>
+        <p>
+          <span className="bold-text">Website: </span>
+          {userDetails.website}
+        </p>
+        <hr />
+        <div className="app-user-profile-icons-container">
+          <FaUserEdit size={20} />
+          <IoMdTrash size={20} />
+        </div>
+      </li>
+    );
+  };
+
+  const filteredUsersList = usersInfoList.filter((user) =>
+    user.name.toLowerCase().includes(searchInput.toLowerCase())
+  );
 
   return (
     <div className="app-main-container">
@@ -50,15 +71,26 @@ const App = () => {
       </div>
       <div className="app-content-container">
         <div className="app-content-search-filter-container">
-          <input className="app-content-input" />
+          <input
+            value={searchInput}
+            onChange={(event) => {
+              setSearchInput(event.target.value);
+            }}
+            className="app-content-input"
+            placeholder="Search User"
+          />
           <IoFilter size={24} />
         </div>
         <div className="app-users-content-container">
-          <ul className="app-user-cards-ul">
-            {usersInfoList.map((user) => (
-              <EachUser key={user.id} userDetails={user} />
-            ))}
-          </ul>
+          {filteredUsersList.length === 0 ? (
+            <p style={{ textAlign: "center" }}>No search Results</p>
+          ) : (
+            <ul className="app-user-cards-ul">
+              {filteredUsersList.map((user) => (
+                <EachUser key={user.id} userDetails={user} />
+              ))}
+            </ul>
+          )}
         </div>
       </div>
     </div>
